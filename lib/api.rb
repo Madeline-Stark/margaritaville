e600155545c014f060c3e6fe8884af6d876a2c98
github username <github email address>
2020-10-13 15:56:29 -0700

    End of lecture 2

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,26 @@
 class API
 
+    def self.fetch_drinks
+        url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        hash = JSON.parse(response)
+
+        array_of_drinks = hash["drinks"]
+
+        # strDrink - name
+        # strInstructions
+        # strGlass 
+        # strInstructionsDE
+        # :name, :instructions, :glass, :german_instructions
+
+        array_of_drinks.each do |drink_hash|
+            drink = Drink.new
+            drink.name = drink_hash["strDrink"]
+            drink.instructions = drink_hash["strInstructions"]
+            drink.glass = drink_hash["strGlass"]
+            drink.german_instructions = drink_hash["strInstructionsDE"]
+        end 
+    end
+
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,70 @@
 class CLI
 
+    def start
+        puts "Welcome"
+        API.fetch_drinks
+        self.menu
+    end
+
+    def menu 
+        # give user the option to see list of drinks
+        puts "Would you like to see the list of drinks?"
+        puts "Type 'yes' to continue or any other key to exit"
+
+        user_input = gets.strip.downcase
+
+        # if the user says yes
+
+
+        if user_input == "yes" || user_input == "y"
+            puts "Good choice!"
+            #display the list
+            display_list_of_drinks
+            ask_user_for_drink_choice
+
+            sleep(2)
+            puts "\n"
+
+            menu 
+        else 
+            puts "Goodbye"
+        end
+
+    end 
+
+    def display_list_of_drinks
+        # Access all the drinks
+        # binding.pry
+        # print each one out
+        Drink.all.each.with_index(1) do |drink, index|
+            puts "#{index}. #{drink.name}"
+        end 
+    end
+
+    def ask_user_for_drink_choice
+        # ask user for choice
+        puts "Enter the number of the drink you'd like to know more about"
+        index = gets.strip.to_i - 1
+
+        # index valid? number between 0 and 6
+        until index.between?(0, Drink.all.length - 1)
+            # keep asking for user input
+            puts "Sorry invalid. Choose a valid number"
+            index = gets.strip.to_i - 1
+        end
+        
+        drink_instance = Drink.all[index]
+       
+        display_drink_details(drink_instance)
+    end 
+
+    def display_drink_details(drink)
+        sleep(1)
+        puts "\n"
+        puts drink.name 
+        puts "Glass: " + drink.glass
+        puts "Instructions: " + drink.instructions
+        puts "German Instructions: " + drink.german_instructions
+    end
+
 end
